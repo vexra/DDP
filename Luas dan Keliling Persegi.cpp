7895ac2f5830a92a4e939ae2f6f3cc8dd4eb377c
vexra <112993677+vexra@users.noreply.github.com>
2022-09-08 09:54:07 +0700

    Add files via upload
    DPP
    
    Program luas dan keliling persegi

---
+++ Luas dan Keliling Persegi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// Vex corp
+using namespace std;
+
+int main()
+{
+	int p, l;
+	
+	cin >> p >> l;
+	
+	cout << "luas: " << p*l << endl;
+	cout << "keliling: " << 2*(p+l) << endl;
+	
+	return 0;
+}
