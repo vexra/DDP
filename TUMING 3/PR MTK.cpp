a295de47f120c9736640d54aa40e2ef4b93aa298
vexra <112993677+vexra@users.noreply.github.com>
2022-10-05 21:48:21 +0700

    Create PR MTK.cpp

---
+++ TUMING 3/PR MTK.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a,b,c,d;
+    char ch;
+    
+    cin >> ch >> a >> ch >> b >> ch >> ch >> ch >> c >> ch >> d >> ch;
+    cout << (a+b)*(c-d) << " ";
+    
+    cin >> ch >> a >> ch >> b >> ch >> ch >> ch >> c >> ch >> d >> ch;
+    cout << (a+b)*(c-d) << " ";
+    
+    cin >> ch >> a >> ch >> b >> ch >> ch >> ch >> c >> ch >> d >> ch;
+    cout << (a+b)*(c-d);
+    
+    
+    return 0;
+}
